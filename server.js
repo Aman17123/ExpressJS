e41659693fcc85fd72e0d3c4e5400bdb218e8c1e
wwwam <amannakoti0@gmail.com>
2026-02-22 20:51:47 +0530

    mongo connection

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 const app = require('./app')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config({ path: './config.env' })
+
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD)
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+}).then(con => {
+  console.log(con.connections);
+  console.log('DB connection successful!');
+})
+
+
 
 const port = 3000;
 app.listen(port, () => {
